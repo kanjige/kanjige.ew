d833c12fd9900972915dab644aa15945379d1337
Eric Wong <122705933+kanjige@users.noreply.github.com>
2024-12-28 15:38:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the animation to complete (2 seconds) and then another 2 seconds
+setTimeout(function() {
+    // Redirect to a new page
+    window.location.href = "main.html";
+}, 3000); // 4000 milliseconds = 4 seconds
